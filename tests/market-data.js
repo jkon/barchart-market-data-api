b006712bfbc16005aad6f0754c2f63702633f3a7
Joshua Kon <joshuapkon@gmail.com>
2018-07-27 11:16:41 -0600

    changed the way the lib is setup so that it's not just making a class and adding to the prototype. Now the functions are just part of module.exports and you can set the apiKey with setApiKey() function

--- tests/market-data.js
+++ tests/market-data.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const MarketData = require('../');
+const md = require('../');
 const should = require('should');
 const mocha = require('mocha');
 
@@ -9,11 +9,11 @@ const APIKEY = process.env.APIKEY;
 if (!APIKEY) {
   throw new Error('APIKEY not provided');
 }
+md.setApiKey(APIKEY);
 
 describe('MarketData', function () {
   describe('#getHistory', function () {
     it('should return an Array of data', function (done) {
-      const md = new MarketData(APIKEY);
       md.getHistory('IBM').then(function (history) {
         history.should.be.instanceof(Array);
         console.log(history);
@@ -23,7 +23,6 @@ describe('MarketData', function () {
   });
   describe('#getQuote', function () {
     it('should return an Array of data', function (done) {
-      const md = new MarketData(APIKEY);
       md.getQuote('IBM').then(function (quotes) {
         quotes.should.be.instanceof(Array);
         console.log(quotes);
